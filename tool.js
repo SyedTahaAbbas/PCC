bd0793ceca7e5f7f440510636f40aeb59bcf7af4
SyedTaha <taha.mmtlmu@gmail.com>
2024-07-23 23:57:16 +0200

    tool file updated

--- tool.js
+++ tool.js
@@ -1,11 +1,11 @@
-export const random = (min = 0, max = 1) => {
-	return Math.random() * (max - min) + min
+export const getRandom = (lowerBound = 0, upperBound = 1) => {
+	return Math.random() * (upperBound - lowerBound) + lowerBound;
 }
 
-export const floor = (value) => {
-	return Math.floor(value)
+export const getFloor = (num) => {
+	return Math.floor(num);
 }
 
-export const min = (value1, value2) => {
-	return Math.min(value1, value2)
+export const getMin = (num1, num2) => {
+	return Math.min(num1, num2);
 }
